fa0cd18ff912db6ca6b0d1cc41140da28e84d144
John <jonathan.lefebvre@epitech.eu>
2020-02-08 00:25:35 +0100

    feat build

---
+++ build/precache-manifest.d99004c58fa208376985fc142a3b20dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b9749db9f2ac475f8635c32536ffcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23208ea2fb0fd6b8ad86",
+    "url": "/static/css/main.b21c2c05.chunk.css"
+  },
+  {
+    "revision": "47dd5ac66bdd7ab096fa",
+    "url": "/static/js/2.0a029c44.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.0a029c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23208ea2fb0fd6b8ad86",
+    "url": "/static/js/main.1462afb1.chunk.js"
+  },
+  {
+    "revision": "87aa2a7ec2f6f81d6720",
+    "url": "/static/js/runtime-main.c70aac55.js"
+  }
+]);
